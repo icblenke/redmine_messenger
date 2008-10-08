03188638c937aeb752780f74f1c2979ed2d3df9e
Maciej Szczytowski <mszczytowski@gmail.com>
2008-10-08 21:56:06 +0200

    config and init.rb

--- init.rb
+++ init.rb
@@ -1,16 +1,19 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_jabber do
-  name 'Redmine Messenger plugin'
-  author 'Maciej Szczytowski'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-end
+require 'rubygems'
+require 'yaml'
+require 'xmpp4r'
 
-require_dependency 'redmine_messenger/messenger_holder'
 require_dependency 'redmine_messenger/messenger'
 require_dependency 'redmine_messenger/messengers/mock_messenger'
 require_dependency 'redmine_messenger/messengers/xmpp4r_messenger'
 require_dependency 'redmine_messenger/base'
 
-require File.join(File.dirname(__FILE__), 'app/messengers/test_jabber_messenger.rb')
+require File.join(File.dirname(__FILE__), 'app/model/messenger_receiver.rb')
+
+Redmine::Plugin.register :messenger do
+  name 'Messenger'
+  author 'Maciej Szczytowski'
+  description 'Messenger is a plugin to allow users to communicate with Redmine via XMPP protocol.'
+  version '0.0.1'
+end
