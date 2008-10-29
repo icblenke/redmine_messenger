f4f84439c5c244b5fe85a95611c6e80dfb56fcbc
Maciej Szczytowski <mszczytowski@gmail.com>
2008-10-29 21:05:01 +0100

    issues actions

--- app/messengers/issues_messenger.rb
+++ app/messengers/issues_messenger.rb
@@ -1,68 +1,55 @@
 class IssuesMessenger < RedmineMessenger::Base
 
-  #register_handler :issues do |cmd|
-  #  cmd.group :issues
-  #end
+  register_handler :issues do |cmd|
+    cmd.group :issues
+    cmd.param :name, :type => :string, :required => false, :greedy => true
+  end
   
-  #register_handler :issue do |cmd|
-  #  cmd.group :issues
-  #  cmd.param :issue_id, :type => :integer, :required => true
-  #  cmd.param :name, :type => :string, :required => false, :greedy => true
-  #end
+  register_handler :issue do |cmd|
+    cmd.group :issues
+    cmd.param :issue_id, :type => :integer, :required => true   
+  end
 
   def issues(user, params = {})
-    if issues = Issue.find_by_assigned_to_id(user.user_id)
-      responce = ""
-      issues.each do |issue|
-        responce << "\##{issue.id} #{issue.project.name} - #{issue.subject}\n"
-      end
-      responce
+    # TODO don't show finished issues    
+    unless params[:name].blank?
+      issues = Issue.find(:all, :conditions => ["lower(subject) like lower(?)", "%#{params[:name]}%"])
+      return l(:messenger_command_issues_not_found, params[:name]) if issues.empty?
     else
-      l(:messenger_command_issues_not_found)
+      issues = Issue.find_all_by_assigned_to_id(user.user_id)
+      return l(:messenger_command_issues_assigned_not_found) if issues.empty?
     end
+    
+    projects = {}
+    
+    issues.each do |issue|
+      projects[issue.project.name] ||= []
+      projects[issue.project.name] << issue
+    end
+    
+    responce = l(:messenger_command_issues_found, issues.length) << "\n\n"
+    
+    projects.each do |project, issues|
+      responce << project.humanize << ":\n"
+      issues.each do |issue|
+        responce << "    \##{issue.id} #{issue.subject}\n" 
+      end
+      responce << "\n"
+    end
+     
+    responce        
   end
   
   def issue(user, params = {})
     if issue = Issue.find_by_id(params[:issue_id]) 
-      responce = "\##{issue.id} #{issue.project.name} - #{issue.subject} (#{issue.status.name})"
-      responce << l(:messenger_command_issue_assigned_to) << issue.assigned_to.login << "\n" if issue.assigned_to
-      responce << issue.description if issue.description and issue.description.size > 1
+      responce = "#{issue.project.name.humanize}: \##{issue.id} #{issue.subject} (" << l(:messenger_command_issue_status, issue.status.name.downcase)
+      responce << ", " << l(:messenger_command_issue_assigned_to, issue.assigned_to.login.to_s) if issue.assigned_to
+      responce << ")"
+      responce << "\n\n" << issue.description if issue.description and issue.description.size > 1
       responce
     else
       l(:messenger_command_issue_not_found)
     end
   end
-
-#  General:
-#    hello
-#    show (entry|entries|timer|timers|projects)
-#
-#Entries:
-#    log
-#    unlog
-#
-#Timers:
-#    pause
-#    resume
-#    start
-#    finish
-#    cancel
-#    note
-
-#  Show full details of the entry by entry id.
-#    > show entry <entry id>
-
-#Show things.  (type: 'help show <subcommand>').
-#    > show entries
-#    > show entry <entry id>
-#    > show timers
-#    > show timer <timer id>
-#    > show projects
-
-#  Show last 50 entries by you.
-#    > show entries
-
-#  Start timer for the billing code.
-#    > start <billing code>
 
 end
