3ec81f429a8277d5f244c2a1e4f0f226a209f4f0
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-05 20:41:15 +0100

    assigning issues

--- app/messengers/issues_messenger.rb
+++ app/messengers/issues_messenger.rb
@@ -20,6 +20,13 @@ class IssuesMessenger < RedmineMessenger::Base
     cmd.param :issue_id, :type => :integer, :required => true
     cmd.param :note, :type => :string, :greedy => true, :required => true
   end
+  
+  register_handler :assign do |cmd|
+    cmd.group :issues
+    cmd.param :issue_id, :type => :integer, :required => true
+    cmd.param :user, :type => :string, :required => true
+    cmd.param :note, :type => :string, :greedy => true, :required => false
+  end
 
   def issues(user, params = {})  
     unless params[:name].blank?
@@ -62,18 +69,60 @@ class IssuesMessenger < RedmineMessenger::Base
     end
   end
   
+  def assign(user, params = {})
+    if issue = Issue.find_by_id(params[:issue_id]) 
+      return l(:messenger_command_issue_not_assigned_to_you) unless issue.assigned_to == user.user      
+      
+      user_assing_to = User.find_by_login(params[:user])
+      
+      unless user_assing_to
+        return l(:messenger_command_assing_user_not_found, params[:user])
+      end
+      
+      return l(:messenger_command_assing_already_assigned, user_assing_to.login) if user_assing_to == user.user
+      
+      unless params[:note].blank?
+        unless journal(user.user, issue, params[:note])
+          return l(:messenger_command_comment_not_commented)
+        end
+      end
+      
+      issue.assigned_to = user_assing_to
+      
+      if issue.save
+        l(:messenger_command_assing_assigned, user_assing_to.login)
+      else
+        l(:messenger_command_assing_not_assigned, user_assing_to.login)
+      end
+    else
+      l(:messenger_command_issue_not_found)
+    end
+  end
+  
   def comment(user, params = {})
     if issue = Issue.find_by_id(params[:issue_id]) 
-      journal = issue.init_journal(user.user, params[:note])
+      return l(:messenger_command_issue_not_assigned_to_you) unless issue.assigned_to == user.user
       
-      if journal.save
-        Mailer.deliver_issue_edit(journal) if Setting.notified_events.include?('issue_updated')
+      if journal(user.user, issue, params[:note])
         l(:messenger_command_comment_commented)
       else 
         l(:messenger_command_comment_not_commented)
       end
     else
       l(:messenger_command_issue_not_found)
+    end
+  end
+  
+  private
+  
+  def journal(user, issue, notes)
+    journal = issue.init_journal(user, notes)
+
+    if journal.save
+      Mailer.deliver_issue_edit(journal) if Setting.notified_events.include?('issue_updated')
+      true
+    else
+      false
     end
   end
 
