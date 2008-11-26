aa2d971508a54f49395691f06063591bed770ae8
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-26 18:00:12 +0100

    logging

--- lib/redmine_messenger/messengers/xmpp4r_messenger.rb
+++ lib/redmine_messenger/messengers/xmpp4r_messenger.rb
@@ -6,7 +6,7 @@ module RedmineMessenger
         super(config)
         
         jid = config['jid']
-        jid += '/Redmine' unless jid =~ /\//
+        jid += "/Redmine#{rand(100000)}" unless jid =~ /\//
 
         @client = Jabber::Client.new(Jabber::JID.new(jid))
         
@@ -17,16 +17,21 @@ module RedmineMessenger
         connect
 
         @client.on_exception do |e,client,where|
-          RAILS_DEFAULT_LOGGER.fatal "RedmineMessenger: exception catched '#{e.message}' (#{where.to_s}; trying to reconnect ..."
+          RAILS_DEFAULT_LOGGER.fatal "RedmineMessenger: exception catched '#{e.message}' (#{where.to_s}); trying to reconnect ..."
           sleep 5
           reconnect
         end
         
         @client.add_message_callback do |m|
           unless m.type == :error
-            receive_message("#{m.from.node}@#{m.from.domain}", m.body)
+            begin 
+              RAILS_DEFAULT_LOGGER.debug "RedmineMessenger: receiving message from #{m.from.node}@#{m.from.domain}"
+              receive_message("#{m.from.node}@#{m.from.domain}", m.body)
+            rescue => e
+              RAILS_DEFAULT_LOGGER.error "RedmineMessenger: exception catched while receiving message '#{e.message}'"
+            end
           else
-            raise m.body
+            RAILS_DEFAULT_LOGGER.error "RedmineMessenger: error received '#{m.body}'"
           end
         end
       
@@ -40,12 +45,18 @@ module RedmineMessenger
           new_status = status(pres.priority, pres.type, pres.show)
             
           unless old_status == new_status
-            receive_status("#{item.jid.node}@#{item.jid.domain}", new_status)
+            begin
+              RAILS_DEFAULT_LOGGER.debug "RedmineMessenger: receiving status from #{item.jid.node}@#{item.jid.domain}"
+              receive_status("#{item.jid.node}@#{item.jid.domain}", new_status)
+            rescue => e
+              RAILS_DEFAULT_LOGGER.error "RedmineMessenger: exception catched while receiving status '#{e.message}'"
+            end
           end
         end
       end
 
       def send_message(to, body)
+        RAILS_DEFAULT_LOGGER.debug "RedmineMessenger: sending message to #{to}"
         @client.send(Jabber::Message.new(to, body).set_type(:chat))
       end
 
@@ -53,7 +64,7 @@ module RedmineMessenger
 
       def reconnect
         if @client.is_connected?
-          RAILS_DEFAULT_LOGGER.info "RedmineMessenger: disconnecting"
+          RAILS_DEFAULT_LOGGER.info "RedmineMessenger: disconnecting ..."
           @client.close          
         end        
         connect        
@@ -61,7 +72,7 @@ module RedmineMessenger
       
       def connect
         unless @client.is_connected?
-          RAILS_DEFAULT_LOGGER.info "RedmineMessenger: connecting"
+          RAILS_DEFAULT_LOGGER.info "RedmineMessenger: connecting ..."
           @client.connect(config['host'],config['port'])
           @client.auth(config['password'])
           @client.send(Jabber::Presence.new(:chat, config['message']))
